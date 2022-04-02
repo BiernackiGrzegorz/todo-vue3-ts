bdb5f1241e1d69db6bef45cb1c8ac2e6a8d3591a
Grzegorz Biernacki <biernacki.grzegorz@gmail.com>
2022-04-02 20:03:04 +0200

    Task type and TaskList component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <h1 class="text-5xl font-bold text-red-900">Hello</h1>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3" />
+   <div>
+     <TasksList :tasks="tasks" />
+   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent, ref } from "@vue/runtime-core";
+import TasksList from './components/TasksList.vue'
+import Task from './types/Task'
+
+export default defineComponent({
+  name: 'App',
+  components: { TasksList },
+  setup() {
+    const tasks = ref<Task[]>([
+      {id: '1', description: 'Cook dinnsser', completed: false },
+      {id: '2', description: 'Feed dog', completed: false },
+      {id: '3', description: 'Visit grandpaxkk', completed: false },
+      {id: '4', description: 'Brush teeth', completed: false }
+    ])
+
+    return { tasks }
+  }
+
+})
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
